533a40a8f7baef7ceaae24a4abc177ed1e808dc9
Ife Banire <IfeBanire@yahoo.com>
2019-08-02 22:46:22 -0400

    Added new controllers to the project.

---
+++ Hobbie Tracker/View Controllers/FriendDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FriendDetailViewController.swift
+//  Hobbie Tracker
+//
+//  Created by Lambda_School_Loaner_101 on 7/30/19.
+//  Copyright © 2019 Ife Banire. All rights reserved.
+//
+
+import UIKit
+
+class FriendDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
